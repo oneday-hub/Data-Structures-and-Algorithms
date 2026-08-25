38a6a54545484ebedb7076b069db5a42fea70691
Nagendra Gupta <guptanagendra107@gmail.com>
2026-08-26 01:28:08 +0530

    solution of best to time to buy and sell stock (part 3)

---
+++ c++/Dymamic Programming/topic29.cpp
@@ -0,0 +1,91 @@
+// lecture : 132 : Best time to buy and sell stock (part 3)
+
+/*
+You are given an array prices where prices[i] is the price of a given stock 
+on the ith day.
+
+Find the maximum profit you can achieve. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must 
+sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging 
+multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1.Recurrsion
+int solve1(int index, int buy, vector<int> &prices, int limit){
+    if(index == prices.size()) return 0;
+    if(limit == 0) return 0;
+
+    int profit = 0;
+
+    if(buy){
+        int buykaro = -prices[index] + solve1(index + 1, 0, prices, limit);
+        int skipkaro = 0 + solve1(index + 1, 1, prices, limit);
+        profit = max(buykaro, skipkaro);
+    }
+    else{
+        int sellkaro = prices[index] + solve2(index + 1, 1, prices, limit - 1);
+        int skipkaro = 0 + solve1(index + 1, 0, prices, limit);
+        profit = max(buykaro, skipkaro);
+    }
+    return profit;
+}
+
+// so basically using the 2. Recurrsion + Memorization solution of 
+// Best time to buy and sell stock (part 2)
+
+// 3 parameters are changing index, buy, limit
+
+int solve2(int index, int buy, vector<int>&prices, int limit, vector<vector<vector<int>>> &dp){
+    if(index == prices.size()) return 0;
+    if(limit == 0) return 0;
+     
+    if(dp[index][buy][limit] != -1) return dp[index][buy][limit];
+
+
+    int profit = 0; 
+    if(buy){
+        int buykaro = -prices[index] + solve2(index + 1, 0, prices, limit, dp);
+        int skipkaro = 0 + solve2(index + 1, 1, prices, limit, dp);
+        profit = max(buykaro, skipkaro);
+    }else{
+        int sellkaro = prices[index] + solve2(index + 1, 1, prices, limit - 1, dp);
+        int skipkaro = 0 + solve2(index + 1, 0 prices, limit, dp);
+        profit = max(sellkaro, skipkaro);
+    }
+    return dp[index][buy][limit] = profit;
+}
+
+int maxprofit(vector<int> &prices){
+    // return solve1(0, 1, prices, 2);
+
+vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+    return solve2(0, 1, prices, 2, dp);
+}
+int main() {    
+    
+    return 0;
+}
