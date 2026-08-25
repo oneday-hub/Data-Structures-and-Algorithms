8825b511e268617eb3d9e4dbcd3e7ed44fbb59d4
Nagendra Gupta <guptanagendra107@gmail.com>
2026-08-25 18:51:26 +0530

    space optimized solution

--- c++/Dymamic Programming/topic28.cpp
+++ c++/Dymamic Programming/topic28.cpp
@@ -96,7 +96,33 @@ int solve3(vector<int> &prices){
     return dp[0][1];
 }
 
+// 4. space optimization solution 
 
+int solve4(vector<int> &prices){
+    int n = prices.size();
+
+    vector<int> curr(2, 0);
+    vector<int> next(2, 0);
+
+    for(int index = n - 1; index >= 0; index--){
+        for(int buy = 0; buy <= 1; buy++){
+            int profit = 0;
+
+            if(buy){
+                int buykaro = -prices[index] + next[0];
+                int skipkaro = 0 + next[1];
+                profit = max(buykaro, skipkaro);
+            }else{
+                int sellkaro = prices[index] + next[1];
+                int skipkaro = 0 + next[0];
+                profit = max(sellkaro, skipkaro);
+            }
+            curr[buy] = profit;
+        }
+        next = curr;
+    }
+    return next[1];
+}
 int maxprofit(vector<int> &prices){
     // return solve1(0, 1, prices);
 
